642c8908447a2c0ce0641c5c8506e671d2c59a44
HeBianGu <908293466@qq.com>
2021-12-31 10:58:50 +0800

    111

---
+++ Source/TestAction/TestAction/IdentityExtention.cs
@@ -0,0 +1,28 @@
+﻿
+using TestAction;
+
+namespace System
+{
+    public static class IdentityExtention
+    {
+        ///// <summary>
+        ///// 注册工程保存信息
+        ///// </summary>
+        ///// <param name="service"></param>
+        //public static void AddIdentity(this IServiceCollection service)
+        //{
+        //    service.AddSingleton<IIdentityService, IdentityService>();
+        //}
+
+        ///// <summary>
+        ///// 工程默认配置
+        ///// </summary>
+        ///// <param name="service"></param>
+        //public static void UseProject(this IApplicationBuilder service, Action<IProjectConfig> action)
+        //{
+        //    var find = ServiceRegistry.Instance.GetInstance<IProjectService>();
+
+        //    action?.Invoke(find as IProjectConfig);
+        //}
+    }
+}
---
+++ Source/TestAction/TestAction/IdentityService.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Threading;
+
+namespace TestAction
+{
+    public class IdentityService 
+    {
+        //Random random = new Random();
+        //public bool Login(string name, string password, out string message)
+        //{
+        //    Thread.Sleep(2000);
+        //    message = "操作超时";
+        //    return random.Next(2) == 1;
+        //}
+
+        //public bool Register(string name, string phone, string password)
+        //{
+        //    return true;
+        //}
+    }
+}
